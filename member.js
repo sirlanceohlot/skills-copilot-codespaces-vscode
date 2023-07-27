ecf5b59c0bcdbbe0c11d0bed705a860db6eaf326
Lance McKinnon <62031972+sirlanceohlot@users.noreply.github.com>
2023-07-27 14:07:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var projectsList = document.getElementById("projectsList");
+    var contactList = document.getElementById("contactList");
+    var aboutList = document.getElementById("aboutList");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    memberList.style.backgroundColor = "#f5f5f5";
+    skillsList.style.backgroundColor = "#ffffff";
+    projectsList.style.backgroundColor = "#ffffff";
+    contactList.style.backgroundColor = "#ffffff";
+    aboutList.style.backgroundColor = "#ffffff";
+}
